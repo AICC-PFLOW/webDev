812020c16f375644249e711ab045459d95f60397
Worldofgentleken <djman8787@gmail.com>
2024-08-05 15:11:31 +0900

    메인 페이지 제작

--- kunhee/src/App.js
+++ kunhee/src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Routes, NavLink } from 'react-router-dom';
 import './App.css';
+import MainPageDesktop from './component/Navigation';
 
-function App() {
+// function Home() {
+//   return (
+//     <div>
+//       <h2>PFLOW</h2>
+//     </div>
+//   );
+// }
+
+// function Resume() {
+//   return (
+//     <div>
+//       <h2>이력서 작성</h2>
+//     </div>
+//   );
+// }
+
+// function CompanySearch() {
+//   return (
+//     <div>
+//       <h2>회사 검색</h2>
+//     </div>
+//   );
+// }
+
+// function Login() {
+//   return (
+//     <div>
+//       <h2>Login</h2>
+//     </div>
+//   );
+// }
+
+// function CompanyRecommend() {
+//   return (
+//     <div>
+//       <h2>회사 추천</h2>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <header>
+//         <ul>
+//           <li><NavLink to="/">PFLOW</NavLink></li>
+//           <li><NavLink to="/resume">이력서 작성</NavLink></li>
+//           <li><NavLink to="/companysearch">회사 검색</NavLink></li>
+//           <li><NavLink to="/companyrecommend">회사 추천</NavLink></li>
+//           <li><NavLink to="/login">Login</NavLink></li>
+//         </ul>
+//       </header>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/resume" element={<Resume />} />
+//         <Route path="/companysearch" element={<CompanySearch />} />
+//         <Route path="/companyrecommend" element={<CompanyRecommend />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/*" element={<div>Not Found</div>} />
+//       </Routes>
+//     </div>
+//   );
+// }
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <MainPageDesktop />
+  )
 }
 
 export default App;
---
+++ kunhee/src/page/index.jsx
@@ -0,0 +1,238 @@
+import React from 'react';
+import './index.css';
+
+export default function Main() {
+  return (
+    <div className='main-container'>
+      <div className='landing-page'>
+        <div className='header'>
+          <div className='block'>
+            <div className='logo'>
+              <div className='logo-1'>
+                <div className='logo-icon'>
+                  <div className='group'>
+                    <div className='group-2'>
+                      <div className='vector' />
+                      <div className='vector-3' />
+                    </div>
+                    <div className='vector-4' />
+                  </div>
+                </div>
+                <span className='pflow'>PFLOW</span>
+              </div>
+            </div>
+            <div className='menu'>
+              <div className='nav-menu'>
+                <div className='products'>
+                  <span className='resume-writing'>이력서 작성</span>
+                </div>
+                <div className='solutions'>
+                  <span className='company-recommendation'>회사 추천</span>
+                </div>
+                <div className='resources'>
+                  <span className='company-search'>회사 검색 </span>
+                </div>
+                <div className='pricing'>
+                  <span className='community'>커뮤니티</span>
+                </div>
+              </div>
+              <button className='btn'>
+                <div className='btn-login'>
+                  <span className='login'>Login</span>
+                </div>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='hero-section'>
+          <div className='block-5'>
+            <div className='heading'>
+              <div className='text-block'>
+                <span className='resume-helper'>이력서 작성 도우미</span>
+                <span className='time-saving'>
+                  이력서 쓰는데 시간 버리지 말자! 글을 못써도! 단어가 생각이
+                  안나도!
+                  <br />
+                  AI로 발견하는 나만의 취업 강점 찾기!
+                </span>
+              </div>
+              <button className='btn-free-trial'>
+                <span className='get-started'>Get started</span>
+                <div className='group-6' />
+              </button>
+            </div>
+            <div className='image-container'>
+              <div className='asset' />
+            </div>
+          </div>
+        </div>
+        <div className='work-management'>
+          <div className='block-7'>
+            <div className='content'>
+              <div className='headline'>
+                <div className='text-block-8'>
+                  <div className='element' />
+                  <span className='company-recommendation-9'>회사 추천</span>
+                  <span className='chatbot-company'>
+                    챗봇 AI로 찾는 나를 위한 회사!
+                  </span>
+                </div>
+                <button className='btn-get-started'>
+                  <span className='get-started-a'>Get Started</span>
+                  <div className='icon'>
+                    <div className='group-b' />
+                  </div>
+                </button>
+              </div>
+              <div className='image-container-c' />
+            </div>
+            <div className='content-d'>
+              <div className='work-together-image'>
+                <div className='ellipse' />
+                <div className='group-e'>
+                  <div className='ellipse-f' />
+                </div>
+                <div className='group-10'>
+                  <div className='ellipse-11' />
+                </div>
+                <div className='group-12' />
+                <div className='ellipse-13' />
+                <div className='group-14'>
+                  <div className='logo-icon-15'>
+                    <div className='group-16'>
+                      <div className='group-17'>
+                        <div className='vector-18' />
+                        <div className='vector-19' />
+                      </div>
+                      <div className='vector-1a' />
+                    </div>
+                  </div>
+                  <div className='rectangle' />
+                </div>
+                <div className='group-1b'>
+                  <div className='ellipse-1c' />
+                </div>
+                <div className='group-1d'>
+                  <div className='ellipse-1e' />
+                </div>
+                <div className='group-1f'>
+                  <div className='ellipse-20' />
+                </div>
+                <div className='group-21' />
+                <div className='group-22'>
+                  <div className='ellipse-23' />
+                </div>
+                <div className='group-24'>
+                  <div className='ellipse-25' />
+                </div>
+              </div>
+              <div className='headline-26'>
+                <div className='text-block-27'>
+                  <div className='element-28' />
+                  <span className='company-search-29'>회사 검색</span>
+                  <span className='chatbot-find-company'>
+                    챗봇으로 내가 원하는 조건에 맞는 회사 찾기!
+                    <br />
+                  </span>
+                </div>
+                <button className='btn-get-started-2a'>
+                  <span className='get-start'>Get Start</span>
+                  <div className='icon-2b'>
+                    <div className='group-2c' />
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='customise-needs'>
+          <div className='block-2d'>
+            <div className='heading-2e'>
+              <div className='text-block-2f'>
+                <div className='element-30' />
+                <span className='share-job-story'>
+                  함께 나누는
+                  <br />
+                  취업 이야기
+                </span>
+                <span className='job-community'>
+                  취업 고민, 면접후기, 근무 후기 등 취업 관련 커뮤니티
+                </span>
+              </div>
+              <button className='btn-get-started-31'>
+                <span className='lets-go'>Let’s Go</span>
+                <div className='icon-32'>
+                  <div className='group-33' />
+                </div>
+              </button>
+            </div>
+            <div className='illustration'>
+              <div className='image' />
+            </div>
+          </div>
+        </div>
+        <div className='footer'>
+          <div className='content-34'>
+            <div className='info'>
+              <div className='logo-description'>
+                <div className='logo-35'>
+                  <span className='pflow-36'>PFLOW</span>
+                  <div className='logo-icon-37'>
+                    <div className='group-38'>
+                      <div className='group-39'>
+                        <div className='vector-3a' />
+                        <div className='vector-3b' />
+                      </div>
+                      <div className='vector-3c' />
+                    </div>
+                  </div>
+                </div>
+                <span className='success-helper'>
+                  성공적인 취업을 위한 도우미
+                </span>
+              </div>
+              <div className='info-3d'>
+                <span className='resume-writing-3e'>이력서 작성</span>
+              </div>
+              <div className='info-3f'>
+                <span className='company-recommendation-40'>회사 추천</span>
+              </div>
+              <div className='info-41'>
+                <span className='company-search-42'>회사 검색</span>
+              </div>
+              <div className='info-43'>
+                <span className='community-44'>커뮤니티</span>
+              </div>
+            </div>
+            <div className='btm'>
+              <div className='terms-and-condition'>
+                <span className='terms-privacy'>Terms & privacy</span>
+                <span className='security'>Security</span>
+                <span className='status'>Status</span>
+                <span className='aicc-pflow'>©2024 AICC PFlow.</span>
+              </div>
+              <div className='social-icon'>
+                <div className='x-facebook'>
+                  <div className='icon-45' />
+                </div>
+                <div className='twitter'>
+                  <div className='group-46'>
+                    <div className='vector-47' />
+                  </div>
+                </div>
+                <div className='linkedin'>
+                  <div className='vector-48' />
+                  <div className='flex-row-db'>
+                    <div className='vector-49' />
+                    <div className='vector-4a' />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='line' />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
